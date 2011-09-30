c44322cb57b1f81422c8e51eaafdec6816d43c0e
Michael Campbell <eldercampbell1@gmail.com>
2011-09-30 12:53:58 -0600

    First commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+struct NAterritories {
+  int units;
+  string controledby ;
+} ;
+Alsaka, Alberta, Central America, Eastern United States, Greenland, Northwest Territory, Ontario, Qubec, Western United States;
+
+struct SAterritories {
+  int units;
+  string controledby ;
+} ;
+Argentina, Brazil, Peru, Venezuela;
+
+struct EUterritories {
+  int units;
+  string controledby ;
+} ;
+United Kingdom, Iceland, Northern Europe, Scandinavia, Southern Europe, Ukraine, Western Europe;
+
+struct Asiaterritories {
+  int units;
+  string controledby ;
+} ;
+Afganistan, China, India, Irkutsk, Japan, Kamchatka, Middle East, Mongolia, Siam, Siberia, Ural, Yakutsk;
+
+struct Austerritories {
+  int units;
+  string controledby ;
+} ;
+Eastern Austraila, Indonesia, New Guinea, Western Austraila;
+
+struct Afterritories {
+  int units;
+  string controledby ;
+} ;
+Congo, East Africa, Egypt, Madagascar, North Africa, South Africa;
