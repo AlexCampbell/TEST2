d228240d3d3c2450331916583ba5d842e2eb502e
AlexCampbell <Eldercampbell1@gmail.com>
2011-10-13 17:17:45 -0600

    Add some other stoof

--- test.cpp
+++ test.cpp
@@ -15,8 +15,11 @@ public:
 
 class Sectors{
 public:
-
+    bool controled;
 protected:
+    void control(void)
+    void uncontrol(void)
+    bool whetercontroled(void)
 std::set<Territory> countries;
 };
 
@@ -33,6 +36,21 @@ void Territory::unselect(void)
 bool Territory::whetherselected(void)
 {
     return(selected);
+}
+
+void Sectors::control(void)
+{
+    controled = 1;
+}
+
+void Territory::uncontrol(void)
+{
+    controled = 0;
+}
+
+bool Territory::whethercontroled(void)
+{
+    return(controled);
 }
 
  int main(){
